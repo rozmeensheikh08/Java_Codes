90cb32ed8ac185c25e461d3bd4088956d3b5d5b5
Rozmeen Sheikh <rozmeensheikh5@gmail.com>
2025-11-01 18:07:48 +0530

    Byte Example code

---
+++ src/ByteExample.java
@@ -0,0 +1,8 @@
+public class ByteExample {
+    public static void main(String[] args) {
+        // Example of Byte data type (-128 to 127)
+        byte number = 88;
+
+        System.out.println("Byte Number = "+ number);
+    }
+}
